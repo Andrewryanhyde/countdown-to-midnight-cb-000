619b20032c2a997eb318e83a9f9cb5fe000ccac4
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-15 13:55:13 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,13 @@
 #write your code here
-
+def countdown_with_sleep(i)
+  sleep 1
+end
 
 def countdown(i)
 
   while i > 0 do
     puts "#{i} SECOND(S)!"
-    sleep 1
+    countdown_with_sleep(i)
     i -= 1
   end
 
@@ -13,8 +15,4 @@ def countdown(i)
     return "HAPPY NEW YEAR!"
   end
 
-end
-
-def countdown_with_sleep(i)
-  sleep 1
 end
