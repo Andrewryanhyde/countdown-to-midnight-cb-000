a93007108c3ccdd52c6c30aac4dd930de2db875e
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-15 13:38:37 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,11 @@
 #write your code here
 
-def countdown(i) 
-  while i >= 0 do 
-    puts i
+def countdown(i)
+  while i >= 0 do
+    puts "#{i} SECOND(S)!"
     i -= 1
   end
+
+  if i == 0
+    puts "Happy New Year!"
 end
