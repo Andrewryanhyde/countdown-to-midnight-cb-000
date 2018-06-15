ee01d17d40cd24e560bf5b6fe1df07e9e4a66260
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-15 13:52:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,15 +1,11 @@
 #write your code here
 
 
-def countdown_with_sleep(i)
-  sleep(1.second)
-end
-
 def countdown(i)
 
   while i > 0 do
     puts "#{i} SECOND(S)!"
-    sleep(1.second)
+
     i -= 1
   end
 
